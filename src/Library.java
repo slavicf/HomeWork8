afcb01b6515181ba1b416c1b275eb7b5b3c94f43
slavicf <jaroslavfrunt@gmail.com>
2018-03-02 19:42:14 +0200

    Задание - библиотека 1

--- src/Library.java
+++ src/Library.java
@@ -5,44 +5,41 @@ import java.util.concurrent.Semaphore;
 public class Library {
 
     static Random rnd = new Random(); //  declarations
-    static int peopleCount;
-    static int maxAmount;
-    static Semaphore semaphore = new Semaphore(maxAmount);
+    static Semaphore semaphore;
 
-    static void visitLibrary() throws InterruptedException {
-//        if (semaphore.availablePermits() == 0)
-//            System.out.println(Thread.currentThread().getName() + "   ждет входа в библиотеку");
+    static void visitLibrary(int person) throws InterruptedException {
+        if (semaphore.availablePermits() == 0)
+            System.out.println(person + "-й человек ждет входа в библиотеку");
+
         semaphore.acquire();
-        System.out.println(Thread.currentThread().getName() +"   вошел в библиотеку");
+        System.out.println(person + "-й человек вошел в библиотеку");
 
         int time = rnd.nextInt(4000) + 1000;
-        System.out.println(Thread.currentThread().getName() +"   читает книгу " + ((float) time / 1000));
+        System.out.println(person + "-й человек читает книгу ");
         Thread.sleep(time);
 
         semaphore.release();
-        System.out.println(Thread.currentThread().getName() +"   вышел из библиотеки");
+        System.out.println(person + "-й человек вышел из библиотеки");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\nВведите количество людей: ");
-        peopleCount = scanner.nextInt();
-        System.out.print("\nВведите вместимость библиотеки: ");
-        maxAmount = scanner.nextInt();
+        System.out.println("Введите количество людей: ");
+        int peopleCount = scanner.nextInt();
+        System.out.println("Введите вместимость библиотеки: ");
+        int maxAmount = scanner.nextInt();
+        semaphore = new Semaphore(maxAmount);
 
         for(int i = 0; i < peopleCount; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        visitLibrary();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            final int PERSON = i + 1;
+            new Thread(() -> {
+                try {
+                    visitLibrary(PERSON);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }).start();
-            System.out.println(i+1 + " человек" + " пришел ко входу в библиотеку.");
+            System.out.println(i+1 + "-й человек" + " пришел ко входу в библиотеку.");
         }
-
     }
 }
