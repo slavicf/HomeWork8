6fe3a6e1f0739bea77950012fa350c9f36f0a9b8
slavicf <jaroslavfrunt@gmail.com>
2018-03-02 20:18:02 +0200

    Задание - библиотека 2

--- src/Library.java
+++ src/Library.java
@@ -5,21 +5,56 @@ import java.util.concurrent.Semaphore;
 public class Library {
 
     static Random rnd = new Random(); //  declarations
-    static Semaphore semaphore;
+    static Semaphore semLib;
+    static Semaphore semDoor = new Semaphore(1);
+
+    static void libraryDoor(int person, boolean direction) throws InterruptedException {
+        int time = 500;
+
+        if (direction) {
+            System.out.println(person + "-й человек подошел к двери с улицы");
+            semDoor.acquire();
+            System.out.println(person + "-й человек проходит через дверь внутрь");
+            Thread.sleep(time);
+            System.out.println(person + "-й человек прошел через дверь внутрь");
+            semDoor.release();
+            new Thread(() -> {
+                try {
+                    visitLibrary(person);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        } else {
+            System.out.println(person + "-й человек подошел к двери изнутри");
+            semDoor.acquire();
+            System.out.println(person + "-й человек проходит через дверь наружу");
+            Thread.sleep(time);
+            System.out.println(person + "-й человек прошел через дверь наружу");
+            semDoor.release();
+        }
+
+    }
 
     static void visitLibrary(int person) throws InterruptedException {
-        if (semaphore.availablePermits() == 0)
+        System.out.println(person + "-й человек" + " пришел ко входу в библиотеку.");
+        if (semLib.availablePermits() == 0)
             System.out.println(person + "-й человек ждет входа в библиотеку");
 
-        semaphore.acquire();
+        semLib.acquire();
         System.out.println(person + "-й человек вошел в библиотеку");
-
         int time = rnd.nextInt(4000) + 1000;
         System.out.println(person + "-й человек читает книгу ");
         Thread.sleep(time);
-
-        semaphore.release();
         System.out.println(person + "-й человек вышел из библиотеки");
+        semLib.release();
+        new Thread(() -> {
+            try {
+                libraryDoor(person, false);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
@@ -28,18 +63,17 @@ public class Library {
         int peopleCount = scanner.nextInt();
         System.out.println("Введите вместимость библиотеки: ");
         int maxAmount = scanner.nextInt();
-        semaphore = new Semaphore(maxAmount);
+        semLib = new Semaphore(maxAmount);
 
         for(int i = 0; i < peopleCount; i++) {
-            final int PERSON = i + 1;
+            int person = i + 1;
             new Thread(() -> {
                 try {
-                    visitLibrary(PERSON);
+                    libraryDoor(person, true);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }).start();
-            System.out.println(i+1 + "-й человек" + " пришел ко входу в библиотеку.");
         }
     }
 }
